19ffa64306ce90ce5a19a00062316a8a4718eba7
Kenneth Vasquez <vasquezkennethb@gmail.com>
2024-10-12 08:25:39 +0800

    Remodeled the database and added functions to pull records

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import os
 import sqlite3
 
 class Model:
@@ -6,27 +7,37 @@ class Model:
     def __init__(self):
         self.conn = sqlite3.connect('Expenses.db')
         self.c = self.conn.cursor()
-        self.category = [
-            "502 TAXES",
-            "503 FIDUCIARY FEES & SERVICES",
-            "504 CONTRIBUTIONS 505 ACCOUNTING & TAX PREPARATION",
-            "506 LEGAL SERVICES 31 CHART OF ACCOUNTS EXPENSES - OTHER"
-        ]
+        self.c.execute("""SELECT name FROM sqlite_master WHERE type='table';""")
 
-        # Create class table
+        if len(self.c.fetchall()) == 0:
+            self.create_class_tbl()
+            self.create_subclass_tbl()
+            self.create_keywords_tbl()
+            self.conn.commit()
+
+        # self.category = [
+        #     "502 TAXES",
+        #     "503 FIDUCIARY FEES & SERVICES",
+        #     "504 CONTRIBUTIONS 505 ACCOUNTING & TAX PREPARATION",
+        #     "506 LEGAL SERVICES 31 CHART OF ACCOUNTS EXPENSES - OTHER"
+        # ]
+
+    def create_class_tbl(self):
+        """Creates class table and inserts default values"""
         self.c.execute("""CREATE TABLE IF NOT EXISTS class (
-                       class_id INTEGER PRIMARY KEY,
-                       class_name TEXT)""")
-        #Insert classes
+                        class_id INTEGER PRIMARY KEY,
+                        class_name TEXT)""")
+
         self.c.execute("""INSERT OR REPLACE INTO class (class_id, class_name)
-                       VALUES (1, 'PERSONAL'), (2, 'TRUST')""")
+                        VALUES (1, 'PERSONAL'), (2, 'TRUST')""")
 
-        #Create subclass table
+
+    def create_subclass_tbl(self):
+        """Creates subclass table and inserts default values"""
         self.c.execute("""CREATE TABLE IF NOT EXISTS subclass (
-                       subclass_id INTEGER PRIMARY KEY,
-                       subclass_name TEXT)""")
+                        subclass_id INTEGER PRIMARY KEY,
+                        subclass_name TEXT)""")
 
-        #Insert Subclasses
         with open("subclasses.txt", "r", encoding="utf-8") as f:
             for idx, subclass in enumerate(f):
                 sql = """INSERT OR REPLACE INTO subclass (
@@ -34,27 +45,41 @@ class Model:
                 VALUES(?, ?)"""
                 self.c.execute(sql, (idx + 1, subclass.strip()))
 
-        #Create keyword table
+    def create_keywords_tbl(self):
+        """Creates keywords table"""
         self.c.execute("""CREATE TABLE IF NOT EXISTS keywords (
-                       keyword_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                       keyword_name TEXT,
-                       category INTEGER,
-                       class_id INTEGER,
-                       subclass_id INTEGER,
-                       FOREIGN KEY(class_id) REFERENCES class(class_id),
-                       FOREIGN KEY(subclass_id) REFERENCES subclass(subclass_id))""")
+                        keyword_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        keyword_name TEXT,
+                        class_id INTEGER,
+                        subclass_id INTEGER,
+                        FOREIGN KEY(class_id) REFERENCES class(class_id),
+                        FOREIGN KEY(subclass_id) REFERENCES subclass(subclass_id))""")
 
-        #Create expenses table
-        self.c.execute("""CREATE TABLE IF NOT EXISTS expenses (
-                       expense_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                       date TEXT,
-                       description TEXT,
-                       amount REAL,
-                       account TEXT,
-                       transaction_type INTEGER,
-                       class_id INTEGER,
-                       subclass_id INTEGER,
-                       FOREIGN KEY(class_id) REFERENCES class(class_id),
-                       FOREIGN KEY(subclass_id) REFERENCES subclass(subclass_id))""")
+    def get_all_keywords(self):
+        sql = """SELECT keyword_name, class_name, subclass_name,
+        LENGTH(keyword_name) as lngt FROM keywords
+        INNER JOIN class ON keywords.class_id = class.class_id
+        INNER JOIN subclass ON keywords.subclass_id = subclass.subclass_id
+        ORDER BY lngt DESC"""
 
+        self.c.execute(sql)
+
+        return self.c.fetchall()
+
+    def get_class_name(self, class_id):
+        sql = """SELECT class_name FROM class WHERE class_id=? """
+        self.c.execute(sql, (class_id, ))
+        return self.c.fetchone()
+
+    def get_subclass_name(self, subclass_id):
+        sql = """SELECT subclass_name FROM subclass WHERE subclass_id=? """
+        self.c.execute(sql, (subclass_id, ))
+        return self.c.fetchone()
+
+    def add_keyword(self, keyword_name, class_id, subclass_id):
+        sql = """INSERT INTO keywords (
+        keyword_name, class_id, subclass_id)
+        VALUES (?, ?, ?)"""
+
+        self.c.execute(sql, (keyword_name, class_id, subclass_id))
         self.conn.commit()
